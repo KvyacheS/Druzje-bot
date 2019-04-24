a21f0f8814af70bd3f495d2565168b111d7bf76f
druzj <44115272+druzj@users.noreply.github.com>
2019-04-24 20:58:29 +0300

    fix

--- THH_bot_py/main.py
+++ THH_bot_py/main.py
@@ -1,8 +1,8 @@
 import logging
+import datetime
 
 from time import time
 
-import datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, BaseFilter
 
@@ -16,18 +16,17 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 
-def hi(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+def aboutGroup(update, context):
+    update.message.reply_photo(photo=open("C:/Users/Alexander's PC/Desktop/THH_bot_py/vg_under.jpg", 'rb'))
 
 
 def slu4ajnijFakt(update, context):
     update.message.reply_text('Сырник - токсичный хуй')
 
 
-tjano4ki = ["ТЯНОЧКУ БЫ", "ТЯНОЧКУ ХОЧУ", "ХОЧУ ТЯНОЧКУ", "ЩАС БЫ ТЯНОЧКУ", "ТЯНОЧКУ ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯНОЧКУ"
-            "ТЯНУЧКУ БЫ", "ТЯНУЧКУ ХОЧУ", "ХОЧУ ТЯНУЧКУ", "ЩАС БЫ ТЯНУЧКУ", "ТЯНУЧКУ ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯНУЧКУ"
-            "ТЯН БЫ", "ТЯН ХОЧУ", "ТЯН ТЯНОЧКУ", "ЩАС БЫ ТЯН", "ТЯН ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯН"
+tjano4ki = ["ТЯНОЧКУ БЫ", "ТЯНОЧКУ ХОЧУ", "ХОЧУ ТЯНОЧКУ", "ЩАС БЫ ТЯНОЧКУ", "ТЯНОЧКУ ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯНОЧКУ",
+            "ТЯНУЧКУ БЫ", "ТЯНУЧКУ ХОЧУ", "ХОЧУ ТЯНУЧКУ", "ЩАС БЫ ТЯНУЧКУ", "ТЯНУЧКУ ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯНУЧКУ",
+            "ТЯН БЫ", "ТЯН ХОЧУ", "ТЯН ТЯНОЧКУ", "ЩАС БЫ ТЯН", "ТЯН ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯН",
             "ТЯНКУ БЫ", "ТЯНКУ ХОЧУ", "ХОЧУ ТЯНКУ", "ЩАС БЫ ТЯНКУ", "ТЯНКУ ХОЧЕТСЯ", "ХОЧЕТСЯ ТЯНКУ"]
 
 
@@ -45,12 +44,12 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-now = datetime.datetime.now()
+def noyou(update, context):
+    update.message.reply_text('нет ты')
 
 
 def unca(update, context):
-    if now.time() > update.message.date:
-        update.message.reply_text("ты бля")
+    update.message.reply_text("не ной, блядь")
 
 
 class TjanFilter(BaseFilter):
@@ -62,6 +61,12 @@ class TjanFilter(BaseFilter):
         return False
 
 
+class NoYouFilter(BaseFilter):
+    def filter(self, message):
+        mes = message.text
+        return 'НЕТ ТЫ' in mes.upper()
+
+
 def main():
     print('Bot started')
     updater = Updater("842084881:AAElZKTtH0ZBFRtOvhsjQlGFv6aXPWdeBnY", use_context=True)
@@ -69,18 +74,19 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    my_filter = TjanFilter()
+    tjan_filter = TjanFilter()
+    no_you_filter = NoYouFilter()
 
-    # dp.add_handler(CommandHandler("hi", hi))
-    # dp.add_handler(MessageHandler(Filters.text, tjan))
-    dp.add_handler(MessageHandler(my_filter, tjan))
-    # dp.add_handler(MessageHandler(Filters.user(username='@DruzjDruzishka'), unca))
-    # dp.add_handler(MessageHandler(Filters.user(username='@SMR_Konstantin_Sosnin'), unca))
+    dp.add_handler(CommandHandler("aboutGroup", aboutGroup))
+    dp.add_handler(MessageHandler(tjan_filter, tjan))
+    dp.add_handler(MessageHandler(Filters.user(username='@Caendaley'), unca))
+    # DruzjDruzishka Caendaley
+    dp.add_handler(MessageHandler(no_you_filter, noyou))
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_polling(clean=True)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
